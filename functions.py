c6df182c38486a1d4ac7d4ed64dacb45b9fb28f3
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-24 14:45:52 +1100

    Added comments to functions.py code for easier understanding
    Added comments to functions.py code for easier understanding

--- functions.py
+++ functions.py
@@ -1,10 +1,12 @@
 import requests
 
-country_data = input("Enter a country you would like to view information on: ")
+country_data = input("Enter a country you would like to view information on: ") # enter country here prompt 
 
-url = f"https://restcountries.com/v3.1/name/{country_data}"
+url = f"https://restcountries.com/v3.1/name/{country_data}" # api link where data is retrieved
 
-response = requests.get(url)
+response = requests.get(url) # response created to check if error occurs or if data is collected
 
-if response.status_code == 200:
+if response.status_code == 200: # if response code = 200 data is successfully collected.
     data = response.json()
+
+# this part of the code works
