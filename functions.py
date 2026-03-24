3bb9a39df85c2ea8baaa09e84961816e5880fd01
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-24 14:09:25 +1100

    Add country_data into functions.py along with API url and response code (see desc.)
    Added country_data variable, url variable, response variable and if statement scanning if data recieved successfully ('code 200')

--- functions.py
+++ functions.py
@@ -1,1 +1,10 @@
 import requests
+
+country_data = input("Enter a country you would like to view information on: ")
+
+url = f"https://restcountries.com/v3.1/name/{country_data}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
