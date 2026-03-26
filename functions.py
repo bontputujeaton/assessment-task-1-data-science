e3e6b23fb2fd6b9f4aa2c4cf23c88fa4ec93536d
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 22:20:16 +1100

    Testing show_countrydata error

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+# Import a
 import requests
 import pandas
 import matplotlib
@@ -8,7 +9,8 @@ def get_countrydata(): # Retrieve country data from the API. This includes the A
  
     url = f"https://restcountries.com/v3.1/name/{country_data}" # api link where data is retrieved
     
-    try: # Added try statement holding all API response data that is retrieved off user input. This is stored.
+    # Added try statement holding all API response data that is retrieved off user input. This is stored.
+    try: 
         response = requests.get(url, timeout=3) # response created to check if error occurs or if data is collected
 
         if response.status_code == 200: # if response code = 200 data is successfully collected.
@@ -21,16 +23,28 @@ def get_countrydata(): # Retrieve country data from the API. This includes the A
             "capital": data.get("capital", [""])[0],
             "population": data.get("population", ""),
             "region": data.get("region", ""),
-            "flag": data.get("flags", {}).get("png", "")
+            "flag": data.get("flag", {}).get("png", "")
         }
 
     except:
         return None
-    
-    
-# Define display_country to display all the data stored from the API response to the user
 
-def show_countrydata()
+
+# Define display_country to display all the data stored from the API response to the user
+def show_countrydata():
+    print("\n Here's some information about["name"]: ")
+    print("Country Name:", data["name"])
+    print("Capital:", data["capital"])
+    print("Population:", data["population"])
+    print("Region:", data["region"])
+    print("Flag:", data["flag"])
+    print(" \n")
+
+
+        
+      
+
+
 
 
 
