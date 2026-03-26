37575f19fc2e7401ee7f134e9dbb5f5031641a4c
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 19:21:07 +1100

    Define help function and menu function
    Define help function and menu function

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ def countryapp_menu():
     print("Welcome to the Country Data Generator!")
     print("In this application you can view data on any city in the world.")
    #time.sleep(5)
-    print("What would you like to do?")
     print("1. Search for a country's details")
     print("2. View the session history")
     print("3. Save session to the file")
     print("4. Get Help and Extra Information")
     print("5. Exit the program")  
+
+def countryapp_help():
+    print("USER HELP")
+    print("Enter the name of any country to recieve its information. (#1)")
+    print("You can view previous searches in 'View session history' (#2)")
+    print("For any other option, type the menu number to choose it.")
+
+while True:
+    countryapp_menu()
+    user_choice = input("What would you like to do?")
 
