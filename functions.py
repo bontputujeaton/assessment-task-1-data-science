e82877a6b247437e33697c7dfd514a8b6283cf6d
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 19:46:02 +1100

    Defined get_country_data including the API url
    define get_country_data

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
 import requests
 import pandas
 import matplotlib
+import time
 from datetime import datetime
 
-country_data = input("Enter a country you would like to view information on: ") # enter country here prompt 
-
-url = f"https://restcountries.com/v3.1/name/{country_data}" # api link where data is retrieved
+def get_country_data():
+ 
+ url = f"https://restcountries.com/v3.1/name/{country_data}" # api link where data is retrieved
 
 response = requests.get(url, timeout=3) # response created to check if error occurs or if data is collected
 
