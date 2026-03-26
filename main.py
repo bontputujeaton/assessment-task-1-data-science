705a6a70db9246ffb781bd809c4f1f270fbc5359
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 19:15:35 +1100

    Editing main.py - imported time and progress on user interface
    edit main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # put input prompts and print prompts here from functions.py
-from functions import * # import everything from functions.py into main.py
+from functions import time # import everything from functions.py into main.py
 
 def countryapp_menu():
     print("Welcome to the Country Data Generator!")
-    print("")
+    print("In this application you can view data on any city in the world.")
+   #time.sleep(5)
+    print("What would you like to do?")
     print("1. Search for a country's details")
     print("2. View the session history")
     print("3. Save session to the file")
     print("4. Get Help and Extra Information")
-    print("5. Exit the program")
+    print("5. Exit the program")  
 
-def countryhelp_menu():
-
-
-    
