c4952360c9700f4f9381d2482790e482b25d46b0
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 22:32:16 +1100

    Added necessary comments in lines and fixed syntax error

--- functions.py
+++ functions.py
@@ -1,21 +1,25 @@
-# Import a
-import requests
-import pandas
-import matplotlib
-import time
-from datetime import datetime
+# Import all dependencies
+import requests # Requests for API handling
+import pandas # Pandas for data analysis
+import matplotlib # Matplotlib for visualisation (may not be needed in final product)
+import time # Standard python time module
+from datetime import datetime # More advanced than regular time module (also may not be needed in final product)
 
-def get_countrydata(): # Retrieve country data from the API. This includes the API link where the data is collected from
- 
-    url = f"https://restcountries.com/v3.1/name/{country_data}" # api link where data is retrieved
+# Retrieve country data from the API. This includes the API link where the data is collected from
+def get_countrydata(): 
+    # api link where data is retrieved
+    url = f"https://restcountries.com/v3.1/name/{country_data}" 
     
     # Added try statement holding all API response data that is retrieved off user input. This is stored.
     try: 
-        response = requests.get(url, timeout=3) # response created to check if error occurs or if data is collected
+        # response created to check if error occurs or if data is collected
+        response = requests.get(url, timeout=3) 
 
-        if response.status_code == 200: # if response code = 200 data is successfully collected.
+        # if response code = 200 data is successfully collected.
+        if response.status_code == 200: 
             return None
         
+
         data = response.json()
 
         return {
@@ -32,13 +36,23 @@ def get_countrydata(): # Retrieve country data from the API. This includes the A
 
 # Define display_country to display all the data stored from the API response to the user
 def show_countrydata():
-    print("\n Here's some information about["name"]: ")
+    print(f"\n Here's some information about", data["name"], ":")
     print("Country Name:", data["name"])
+    time.sleep(0.5)
     print("Capital:", data["capital"])
+    time.sleep(0.3)
     print("Population:", data["population"])
+    time.sleep(0.3)
     print("Region:", data["region"])
+    time.sleep(0.3)
     print("Flag:", data["flag"])
-    print(" \n")
+    time.sleep(0.1)
+    print("\n")
+
+# Save all the user's file data (information recieved from the API) to another file (country_history.txt)
+
+def save_countrydata():
+    with open
 
 
         
