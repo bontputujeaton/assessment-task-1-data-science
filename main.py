cf8e8f37c6bd29a78d2b307422df203beabc8b6c
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 14:11:18 +1100

    Define country app menu and started country help menu
    search for country menu

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # put input prompts and print prompts here from functions.py
+from functions import * # import everything from functions.py into main.py
 
-from functions import get
+def countryapp_menu():
+    print("Welcome to the Country Data Generator!")
+    print("")
+    print("1. Search for a country's details")
+    print("2. View the session history")
+    print("3. Save session to the file")
+    print("4. Get Help and Extra Information")
+    print("5. Exit the program")
+
+def countryhelp_menu():
+
+
+    
