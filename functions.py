6ef2a69f603ff35705ea33d437562e4042a328ef
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 14:19:29 +1100

    Added timeout of 3 seconds to request response
    Added timeout of 3 seconds to request response

--- functions.py
+++ functions.py
@@ -3,14 +3,15 @@ import pandas
 import matplotlib
 from datetime import datetime
 
-
 country_data = input("Enter a country you would like to view information on: ") # enter country here prompt 
 
 url = f"https://restcountries.com/v3.1/name/{country_data}" # api link where data is retrieved
 
-response = requests.get(url) # response created to check if error occurs or if data is collected
+response = requests.get(url, timeout=3) # response created to check if error occurs or if data is collected
 
 if response.status_code == 200: # if response code = 200 data is successfully collected.
     data = response.json()
 
 # this part of the code works
+
+
