1bbc9851ececf3c3a3381200fb27b894a717222f
bontputujeaton <261820344+bontputujeaton@users.noreply.github.com>
2026-03-26 19:40:01 +1100

    Added user_choice, country input, data_result and functions to add
    Added:
    user_choice
    data_result
    show_country (add later)
    save_history (add later)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # put input prompts and print prompts here from functions.py
-from functions import time # import everything from functions.py into main.py
+from functions import * 
+# import everything from functions.py into main.py
 
 def countryapp_menu():
     print("Welcome to the Country Data Generator!")
     print("In this application you can view data on any city in the world.")
-   #time.sleep(5)
+    time.sleep(5)
     print("1. Search for a country's details")
     print("2. View the session history")
     print("3. Save session to the file")
@@ -21,3 +22,14 @@ while True:
     countryapp_menu()
     user_choice = input("What would you like to do?")
 
+    if user_choice == "1":
+        country = input("Enter the name of a country: ")
+        
+    data_result = get_country_data(country.lower())
+
+    if data_result:
+        show_country(data_result) # this will be added as a function later
+        save_history(data_result) # this will also be added as a function later
+    else:
+
+        
